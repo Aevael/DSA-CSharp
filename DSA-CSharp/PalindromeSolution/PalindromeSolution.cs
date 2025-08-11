a6c0fca689af17d17f3c9023b7c7b885e21eb44f
Aevael <98851230+Aevael@users.noreply.github.com>
2025-08-11 18:59:49 -0500

    Add project files.

---
+++ DSA-CSharp/PalindromeSolution/PalindromeSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_CSharp.PalindromeSolution
+{
+    internal class PalindromeSolution
+    {
+        public bool IsPalindrome(int x)
+            {
+                var numberString = x.ToString();
+                var numberArray = numberString.ToCharArray();
+                Array.Reverse(numberArray);
+                var reverseString = new string(numberArray);
+                return numberString.Equals(reverseString);
+            }
+    }
+}
---
+++ DSA-CSharp/SolutionRunner.cs
@@ -0,0 +1,6 @@
+﻿using DSA_CSharp.PalindromeSolution;
+
+PalindromeSolution solution = new PalindromeSolution();
+Console.WriteLine(solution.IsPalindrome(121));
+Console.WriteLine("Press any key to terminate program.");
+Console.ReadKey();
